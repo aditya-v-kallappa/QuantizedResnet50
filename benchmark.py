cb2e65a616d0d0eb59f6702dbfc1f7c207b67a23
aditya-v-kallappa <67639476+aditya-v-kallappa@users.noreply.github.com>
2020-12-15 10:59:03 +0530

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 from torchvision.models.utils import load_state_dict_from_url
 from torch.quantization import QuantStub, DeQuantStub, fuse_modules
 from torch._jit_internal import Optional
+import time
 
 class QuantizableBasicBlock(BasicBlock):
     def __init__(self, *args, **kwargs):
@@ -104,8 +105,27 @@ class QuantizableResNet(ResNet):
             if type(m) == QuantizableBottleneck or type(m) == QuantizableBasicBlock:
                 m.fuse_model()
 
-def quantize_model(model, backend):
-    _dummy_input_data = torch.rand(1, 3, 299, 299)
+
+def view_model(model):
+    # print(model)
+    for m in model.modules():
+        print(type(m))
+        if type(m) == torch.nn.quantized.BatchNorm2d:
+            print(type(m.weight()), m.weight().zero_point, m.weight().scale)
+            print(m.weight().int_repr())
+    return model
+
+
+
+
+def quantize_model(model, backend, loader):
+    # for i, data in enumerate(testloader, 0):
+    #     _dummy_input_data = data[0]
+    #     if i == 0:
+    #         break
+    _iterator = iter(loader)
+    # print(next(_dummy_input_data))
+    _dummy_input_data = next(_iterator)[0]
     if backend not in torch.backends.quantized.supported_engines:
         raise RuntimeError("Quantized backend not supported ")
     torch.backends.quantized.engine = backend
@@ -132,26 +152,39 @@ def benchmark(model, testloader):
     criterion = nn.CrossEntropyLoss()
     n_batches = 0
     test_loss = 0
-    accuracy = 0
+    top1 = 0
+    top2 = 0
+    top5 = 0
     device = 'cpu'
-
+    elapsed_time = 0
     for i, data in enumerate(testloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         n_batches += 1
         inputs, labels = data[0].to(device), data[1].to(device)
         # if quant == True:
-        #     inputs = torch.quantize_per_tensor(inputs, scale=1e-3, zero_point=128, dtype=torch.quint8)
-            # labels = torch.quantize_per_tensor(labels, scale=1e-3, zero_point=128, dtype=torch.quint8)
-
+            #     inputs = torch.quantize_['conv1', 'bn1', 'relu']per_tensor(inputs, scale=1e-3, zero_point=128, dtype=torch.quint8)
+                # labels = torch.quantize_per_tensor(labels, scale=1e-3, zero_point=128, dtype=torch.quint8)
+        start_time = time.time()
         outputs = model(inputs)
+        end_time = time.time()
+        elapsed_time += (end_time - start_time)
         test_loss += criterion(outputs, labels).item()
-        accuracy += (F.softmax(outputs, dim=1).argmax(dim=1) == labels).float().mean()
+        temp = F.softmax(outputs, dim=1)
+        top1 += (torch.topk(temp, 1, dim=1).indices[:, 0] == labels).float().mean()
+        top2 += (torch.topk(temp, 2, dim=1).indices[:, 0] == labels).float().mean() + \
+                (torch.topk(temp, 2, dim=1).indices[:, 1] == labels).float().mean()
+        for j in range(5):
+            top5 += (torch.topk(temp, 5, dim=1).indices[:, j] == labels).float().mean()
 
-    accuracy /= n_batches
+    top1 /= n_batches
+    top2 /= n_batches
+    top5 /= n_batches
     test_loss /= n_batches
-    print('Test Loss', test_loss)
-    print('Test Accuracy', accuracy.item())
-
+    # print('Test Loss', test_loss)
+    print('Top 1 Accuracy', top1.item())
+    print('Top 2 Accuracy', top2.item())
+    print('Top 5 Accuracy', top5.item())
+    print(f'Time taken to run model on {n_batches * 100} images: {elapsed_time} sec')
 
 def print_size_of_model(model, label=""):
     torch.save(model.state_dict(), "temp.p")
@@ -166,18 +199,25 @@ testset = torchvision.datasets.CIFAR10(root='./cifar10', train=False, download=F
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 
-model_path = './saved_models/quant_resnet50_CIFAR10_ADAM_100_0.01_100_100.pt'
+model_path = './saved_models/quant_resnet50_CIFAR10_SGD_100_0.01_100_100.pt'
 
 
 float_model = QuantizableResNet(block=QuantizableBottleneck, layers=[3, 4, 6, 3])
 
+
 float_model.load_state_dict(torch.load(model_path))
 print("Float Model(MB):{0:.2f}".format(print_size_of_model(float_model) / 1e6))
-
+# print(float_model)
 print("Test Results for Float Model:")
 benchmark(float_model, testloader)
 
-int_model = quantize_model(float_model, 'fbgemm')
+int_model = quantize_model(float_model, 'fbgemm', testloader)
 print("Quantized Model(MB):{0:.2f}".format(print_size_of_model(int_model) / 1e6))
 print("Test Results for Quantized Model:")
 benchmark(int_model, testloader)
+# view_model(int_model)
+
+# print(float_model)
+# for param_tensor in float_model.state_dict():
+#     if 'bn' in param_tensor:
+#         print(param_tensor, "\t", float_model.state_dict()[param_tensor].size())
